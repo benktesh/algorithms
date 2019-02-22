5165245a6eafcdae419ae46f58a439ca5f68eb86
benktesh <benktesh@yahoo.com>
2019-02-22 08:58:27 -0600

    Completes remove duplicates, reverse words, and string permutation.

---
+++ ReverseWords.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace algorithms
+{
+    /*
+     * Given a String of length S, reverse the whole string without reversing the individual words in it. Words are separated by dots.
+     */
+    public class ReverseWords
+    {
+        public static String Reverse(String data)
+        {
+            String reversed = null;
+
+            try
+            {
+                var reversedArray =  data.Split(new char[] {'.', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => (x)).ToArray().Reverse();
+                reversed = String.Join(".", reversedArray);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("parseToInt: " + ex.StackTrace);
+                return null;
+            }
+            return reversed;
+        }
+
+        public static void RunMe()
+        {
+            string words = "i.like.this.program.very.much";
+            var reversed = Reverse(words);
+            Console.WriteLine(reversed);
+        }
+        
+
+    }
+}
---
+++ StringPermutations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace algorithms
+{
+    class StringPermutations
+    {
+        static HashSet<String> permutation = new HashSet<string>();
+        static void Permutation(String data, String prefix)
+        {
+            //Console.WriteLine($"Permutation {counter}: {data} Prefix: {prefix}");
+            if (data.Length == 0)
+            {
+                //Console.WriteLine($"Perms adding: {prefix}" );
+                permutation.Add(prefix);
+            }
+            else
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    String rem = data.Substring(0, i) + data.Substring(i + 1);
+                    Permutation(rem, prefix + data[i]);
+                }
+            }
+        }
+
+        public static void RunMe()
+        {
+            string word = "BAC";
+            //Sort the characters
+            word = String.Concat(word.OrderBy(c => c));
+            Permutation(word, "");
+            Console.WriteLine(string.Join(" ", permutation));  
+        }
+
+    }
+}
