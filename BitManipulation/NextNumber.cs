3486adb71b72185d6c9e14e7283651e130645902
benktesh <benktesh@yahoo.com>
2019-03-14 13:17:20 -0500

    NextNumber shell code

---
+++ BitManipulation/NextNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms.BitManipulation
+{
+    /*
+     * Next Number: Given a positive integer, print the next smallest and the next largest number that
+       have the same number of 1 bits in their binary representation.
+     */
+    public class NextNumber
+    {
+
+    }
+}
