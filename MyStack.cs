494183639b874559d1bdb177268ce7983b2df861
benktesh <benktesh@yahoo.com>
2019-03-14 06:37:00 -0500

    Adds a simple generic stack

---
+++ MyStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace algorithms
+{
+
+    public class MyStack<T> where T : class
+    {
+        int stackTop = 0;
+        T[] items = new T[100];
+        public void Push(T item)
+        {
+            if (stackTop == items.Length)
+            {
+                throw new Exception("Stack Full");
+            }
+            items[stackTop] = item;
+            stackTop++;
+        }
+
+        public T Pop()
+        {
+            if (stackTop == 0)
+            {
+                throw new Exception("Stack Empty");
+            }
+            stackTop--;
+            return items[stackTop];
+        }
+    }
+
+
+}
