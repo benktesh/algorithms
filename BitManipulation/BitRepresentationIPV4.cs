ccd566d981b6d8e8b5f1fdf93a512dc8da1759fa
benktesh <benktesh@yahoo.com>
2019-06-28 04:25:32 -0500

    Adds an IPV4 32bit parser

---
+++ BitManipulation/BitRepresentationIPV4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Algorithms.BitManipulation
+{
+    public class BitRepresentationIPV4
+    {
+        public String Get32Bit(String ip)
+        {
+            var arr = ip.Split('.');
+            if (arr.Length != 4)
+            {
+                throw new ArgumentException("Invalid IP");
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var oct in arr)
+            {
+                int octInt;
+                if (!Int32.TryParse(oct, out octInt))
+                {
+                    throw new ArgumentException("Invalid Ip");
+                }
+                sb.Append(Convert.ToString(octInt, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+    }
+}
